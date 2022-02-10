1d6bd281bb9af3d3db629d17775a00d252f4c846
404Virus <legit.austin.simons22@gmail.com>
2022-02-09 17:04:23 -0700

    Updated:Html,and JavaScript

--- Frontend_intro_to_DOM_temp_converter/script.js
+++ Frontend_intro_to_DOM_temp_converter/script.js
@@ -1,8 +1,11 @@
 
 function clickedFahrenheit() {
     var input1 = document.getElementById("clickedFahrenheit")
-        
-    
+    var display = document.write("number");
+    var display2  = document.getElementById("displayNum");  
+    var input2 = document.getElementById("submit");  
+    var display = document.write("number");
+    var display2  = document.getElementById("displayNum");
 }
 
 function clickedSubmit() {
