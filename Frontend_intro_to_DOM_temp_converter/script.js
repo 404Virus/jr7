4fa569eb45e06901469fa683dc768a9abfd80b9c
404Virus <legit.austin.simons22@gmail.com>
2022-02-09 19:15:26 -0700

    Add: Frontend_callback_excercises

--- Frontend_intro_to_DOM_temp_converter/script.js
+++ Frontend_intro_to_DOM_temp_converter/script.js
@@ -1,19 +1,46 @@
 
-function clickedFahrenheit() {
-    var input1 = document.getElementById("clickedFahrenheit")
-    var display = document.write("number");
-    var display2  = document.getElementById("displayNum");  
-    var input2 = document.getElementById("submit");  
-    var display = document.write("number");
-    var display2  = document.getElementById("displayNum");
-}
+function converter() {
+    var result;
 
-function clickedSubmit() {
-    var input2 = document.getElementById("submit");
-}
+       var convert = document.getElementById("numb");
 
-function display() {
-    var display = document.write("number");
-    var display2  = document.getElementById("displayNum");
+       var fahrenheit = document.getElementById("clickedFahrenheit")
+       var celsius = document.getElementById("clickedCelsius")
+         if(fahrenheit.checked == true){
+            result = (convert * 9/5 ) + 32;
+         } else {
+             result = ( convert - 32) * 5/9;
+         }
 
-}
+         var display = document.getElementById("displayNum")
+         
+
+    }
+
+
+
+
+
+
+
+// function clickedFahrenheit() {
+//     var input1 = document.getElementById("clickedFahrenheit")
+// }
+// function form1() {
+    
+//     var display = document.write("number");
+//     var display2  = document.getElementById("displayNum");  
+//     var input2 = document.getElementById("submit");  
+//     var display = document.write("number");
+//     var display2  = document.getElementById("displayNum");
+
+
+// function clickedSubmit() {
+//     var input2 = document.getElementById("submit");
+
+
+// function display() {
+//     var display = document.write("number");
+//     var display2  = document.getElementById("displayNum");
+// }
+
